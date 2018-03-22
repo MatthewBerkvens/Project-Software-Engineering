1e5b717877ecde802800cda53e7579b9d2aab415
MatthewVM <matthew@berkvens.com>
2018-03-22 12:10:31 +0100

    first commit, cpp main files, cmakelists.txt, environment, gitignore

---
+++ src/main_debug.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Debug!" << std::endl;
+    return 0;
+}
---
+++ src/main_release.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Release!" << std::endl;
+    return 0;
+}
