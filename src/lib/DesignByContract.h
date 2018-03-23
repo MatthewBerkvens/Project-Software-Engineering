696b06d645adce5cdb39bb4d8d793fb6d5480803
MatthewVM <matthew@berkvens.com>
2018-03-23 10:30:05 +0100

    added libraries tinyxml, designbycontract.h

---
+++ src/lib/DesignByContract.h
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : DesignByContract.h
+// Author      : Serge Demeyer
+// Version     :
+// Copyright   : Project Software Engineering - BA1 Informatica - Serge Demeyer - University of Antwerp
+// Description : Declarations for design by contract in C++
+//============================================================================
+
+#ifndef INC_DESIGNBYCONTRACT_H
+#define INC_DESIGNBYCONTRACT_H
+#include <assert.h>
+
+#define REQUIRE(assertion, what) \
+	if (!(assertion)) __assert (what, __FILE__, __LINE__)
+
+#define ENSURE(assertion, what) \
+	if (!(assertion)) __assert (what, __FILE__, __LINE__)
+
+#endif //INC_DESIGNBYCONTRACT_H
